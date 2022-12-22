9d2428cd823def085fe5821f25ad69c26c26c0ec
Benedict Allerberger <me@bene.dev>
2022-12-22 13:06:17 +0100

    Work on `plugin`

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -6,22 +6,23 @@ import {
   PlatformConfig,
   StaticPlatformPlugin,
 } from "homebridge";
-import { ExampleSwitch } from "./services/Switch";
+import { Switch } from "./services/Switch";
 
 let hap: HAP;
 
 class WebSocketPlatform implements StaticPlatformPlugin {
-  private readonly _log: Logging;
-  private readonly _accessories: AccessoryPlugin[];
+  private readonly _accessories: AccessoryPlugin[] = [];
 
   constructor(log: Logging, config: PlatformConfig, api: API) {
-    this._log = log;
-    this._accessories = [
-      new ExampleSwitch(hap, this._log, "Switch 1"),
-      new ExampleSwitch(hap, this._log, "Switch 2"),
-    ];
-
-    console.log(config);
+    if (config.accessories && Array.isArray(config.accessories)) {
+      for (const accessoryConfig of config.accessories) {
+        if (accessoryConfig.service === "Switch") {
+          this._accessories.push(
+            new Switch(log, hap, accessoryConfig.name, config.apiUrl),
+          );
+        }
+      }
+    }
   }
 
   accessories(callback: (foundAccessories: AccessoryPlugin[]) => void): void {
--- packages/plugin/src/services/Switch.ts
+++ packages/plugin/src/services/Switch.ts
@@ -9,18 +9,15 @@ import {
   Service,
 } from "homebridge";
 
-export class ExampleSwitch implements AccessoryPlugin {
-  private readonly log: Logging;
-
-  private switchOn = false;
-
-  // This property must be existent!!
+export class Switch implements AccessoryPlugin {
+  // This property must be existent
   name: string;
 
+  private readonly log: Logging;
   private readonly switchService: Service;
   private readonly informationService: Service;
 
-  constructor(hap: HAP, log: Logging, name: string) {
+  constructor(log: Logging, hap: HAP, name: string, apiUrl: string) {
     this.log = log;
     this.name = name;
 
@@ -29,20 +26,14 @@ export class ExampleSwitch implements AccessoryPlugin {
       .on(
         CharacteristicEventTypes.GET,
         (callback: CharacteristicGetCallback) => {
-          log.info(
-            "Current state of the switch was returned: " +
-              (this.switchOn ? "ON" : "OFF"),
-          );
-          callback(undefined, this.switchOn);
+          const on = true;
+          callback(undefined, on);
         },
       )
       .on(
         CharacteristicEventTypes.SET,
         (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-          this.switchOn = value as boolean;
-          log.info(
-            "Switch state was set to: " + (this.switchOn ? "ON" : "OFF"),
-          );
+          const on = value as boolean;
           callback();
         },
       );
@@ -50,22 +41,8 @@ export class ExampleSwitch implements AccessoryPlugin {
     this.informationService = new hap.Service.AccessoryInformation()
       .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
       .setCharacteristic(hap.Characteristic.Model, "Custom Model");
-
-    log.info("Example switch '%s' created!", name);
   }
 
-  /*
-     * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-     * Typical this only ever happens at the pairing process.
-     */
-  identify(): void {
-    this.log("Identify!");
-  }
-
-  /*
-     * This method is called directly after creation of this instance.
-     * It should return all services which should be added to the accessory.
-     */
   getServices(): Service[] {
     return [
       this.informationService,
